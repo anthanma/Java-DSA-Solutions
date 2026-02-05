377d645e5870692e77e9b451e27e23363a97d14b
om verma <131939085+anthanma@users.noreply.github.com>
2026-02-05 20:25:57 +0530

    Create Running_Sum_of_1d_Array.java

---
+++ Running_Sum_of_1d_Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int [] sums=new int[nums.length];
+        for(int i =0 ; i<nums.length;i++){
+            if(i==0){
+                sums[i]=nums[i];
+            }
+            else{
+                sums[i]=sums[i-1]+nums[i];
+            }
+        }
+        return sums;
+        
+    }
+}
